897a278928d997c118a5cd9c54c2fd430e0770c3
Sven Gowal <908515+sgowal@users.noreply.github.com>
2019-12-31 16:47:07 +0000

    Camera

--- custom_components/badnest/camera.py
+++ custom_components/badnest/camera.py
@@ -20,6 +20,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 class NestCamera(Camera):
 
   def __init__(self, device):
+    super(self, NestCamera).__init__()
     self._device = device
 
   @property
